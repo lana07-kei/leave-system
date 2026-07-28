7855a248b4ab10fa4d1504d6a1371f571932d01c
lana07-kei <lanakei543@gmail.com>
2026-07-28 22:05:53 +0700

    fix: Auto-set user_id and total_days on leave request creation

--- app/Filament/Resources/LeaveRequestResource/Pages/CreateLeaveRequest.php
+++ app/Filament/Resources/LeaveRequestResource/Pages/CreateLeaveRequest.php
@@ -9,6 +9,14 @@ class CreateLeaveRequest extends CreateRecord
 {
     protected static string $resource = LeaveRequestResource::class;
 
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['user_id'] = auth()->id();
+        $data['total_days'] = \Carbon\Carbon::parse($data['start_date'])
+            ->diffInDays(\Carbon\Carbon::parse($data['end_date'])) + 1;
+        return $data;
+    }
+
     protected function getCreatedNotificationTitle(): ?string
     {
         return 'Pengajuan cuti berhasil diajukan';
